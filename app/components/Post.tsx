192c76e13657457ade9920a9ccb0a4c6773fe8c1
MD. Sabrim Faraz Prottoy <86478493+SFProttoy@users.noreply.github.com>
2024-07-13 23:03:05 +0600

    - set CommentForm

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -47,7 +47,7 @@ export default function Post({ post }: { post: PostProps }) {
               ))}
             </div>
           )}
-          <CommentForm />
+          <CommentForm id={id} />
         </>
       )}
     </>
